70df247952f7ca5c57be6879c4be3ffdf7316e02
Renan Liberato Sant'Ana <renan.libsantana@gmail.com>
2016-06-25 23:46:18 -0300

    Correction of concrete class return

--- src/Auth/Model/Factory/UserTableFactory.php
+++ src/Auth/Model/Factory/UserTableFactory.php
@@ -3,9 +3,10 @@
  * @author Renan Liberato <renan.libsantana@gmail.com>
  */
 
-namespace Auth\Service\TableGateway\Factory;
+namespace Auth\Model\Factory;
 
 use Auth\Model\User;
+use Auth\Model\UserTable;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 use Zend\ServiceManager\ServiceLocatorInterface;
@@ -22,10 +23,12 @@ class UserTableFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $dbAdapter = $serviceLocator->get('authAdapter');
+        $dbAdapter          = $serviceLocator->get('authAdapter');
         $resultSetPrototype = new ResultSet();
         $resultSetPrototype->setArrayObjectPrototype(new User());
 
-        return new TableGateway(User::TABLE, $dbAdapter, null, $resultSetPrototype);
+        $tableGateway = new TableGateway(User::TABLE, $dbAdapter, null, $resultSetPrototype);
+
+        return new UserTable($tableGateway);
     }
 }
